ed07eb814c316cd85a15fcf17a9607cc16836cf7
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2023-04-16 15:59:24 +0600

    banner section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
-        <section>
-            <div>
-                <h2>Lorem Ipsum is simply </h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            </div>
-            <div>
-                <img src="" alt="" />
-            </div>
-        </section>
+        <Banner></Banner>
     );
 };
 
--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <header>
-            <nav className='flex justify-around items-center p-4 border-b-2 border-slate-300'>
+            <nav className='flex justify-around items-center p-4'>
                 <div>
-                    <Link to='/' className='text-xl basis-1/3'>SPX Rocket App</Link>
+                    <Link to='/' className='text-xl font-bold basis-1/3'>SPX Rocket App</Link>
                 </div>
-                <div className='flex justify-end items-center gap-3 basis-2/3'>
-                    <Link to='/' className='text-slate-950' >Home</Link>
-                    <Link to='/login' className='text-slate-950'>Login</Link>
-                    <Link to='/signup' className='text-slate-950'>Signup</Link>
+                <div className='flex justify-end items-center gap-7 basis-2/3 text-slate-950'>
+                    <NavLink to='/' className={({isActive}) => (isActive ? 'text-blue-600' : 'text-slate-950')}>Home</NavLink>
+                    <NavLink to='/login' className={({isActive}) => (isActive ? 'text-blue-600' : 'text-slate-950')}>Login</NavLink>
+                    <NavLink to='/signup' className={({isActive}) => (isActive ? 'text-blue-600' : 'text-slate-950')}>Signup</NavLink>
                 </div>
             </nav>
         </header>
