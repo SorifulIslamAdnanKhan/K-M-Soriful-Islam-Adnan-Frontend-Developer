df87a8161cf2e81efe8323981f7311888c235420
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2023-04-18 02:49:03 +0600

    search functionality added

--- src/Pages/Capsules/Capsules.jsx
+++ src/Pages/Capsules/Capsules.jsx
@@ -14,9 +14,14 @@ const Capsules = () => {
     const handleClick = (capsule) => {
         setIsModalOpen(true);
         setSingleCapsule(capsule);
-      };
+    };
 
     const [capsules, setCapsules] = useState([]);
+
+    const [searchOne, setSearchOne] = useState("");
+    const [searchTwo, setSearchTwo] = useState("");
+    const [searchThree, setSearchThree] = useState("");
+    const [filteredCapsules, setFilteredCapsules] = useState([]);
 
     const url = 'https://api.spacexdata.com/v3/capsules';
 
@@ -24,39 +29,95 @@ const Capsules = () => {
 
         fetch(url)
             .then(res => res.json())
-            .then(data => setCapsules(data))
+            .then(data => {
+                setCapsules(data)
+                setFilteredCapsules(data);
+            })
     }, []);
 
+    const handleSearchOne = (event) => {
+        setSearchOne(event.target.value);
+    };
+
+    const handleSearchTwo = (event) => {
+        setSearchTwo(event.target.value);
+    };
+
+    const handleSearchThree = (event) => {
+        setSearchThree(event.target.value);
+    };
+
+    useEffect(() => {
+        const filteredData = capsules.filter((capsule) => {
+            const filterOne =
+                searchOne === '' ||
+                capsule.status?.toLowerCase().includes(searchOne.toLowerCase());
+            const filterTwo =
+                searchTwo === '' ||
+                capsule.original_launch?.toLowerCase().includes(searchTwo.toLowerCase());
+            const filterThree =
+                searchThree === '' || capsule.type?.toLowerCase().includes(searchThree.toLowerCase());
+            return filterOne && filterTwo && filterThree;
+        });
+        setFilteredCapsules(filteredData);
+    }, [capsules, searchOne, searchTwo, searchThree]);
+
     return (
-        <section className='py-10 p-4'>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4'>
-                {
-                    capsules.map(capsule => <div key={capsule.capsule_serial} className='text-left border p-2 rounded-lg shadow-lg shadow-slate-500/50'>
-                        <p className='text-lg'>Status: {capsule.status}</p>
-                        <p className='text-lg'> Original Launch: {capsule.original_launch}</p>
-                        <p className='text-lg'>Missions: {capsule.missions[0]?.name}</p>
-                        <p className='text-lg'>Landing: {capsule.landings}</p>
-                        <p className='text-lg'>Type: {capsule.type}</p>
-                        <div className='py-6'>
-                            <button onClick={() => handleClick(capsule)} className='w-full rounded-md text-white bg-slate-950 hover:bg-indigo-700 p-4 text-sm'>View Details</button>
-                            <CapsuleModal isOpen={isModalOpen} onClose={handleModalClose} capsule={singleCapsule}></CapsuleModal>
-                        </div>
-                    </div>)
-                }
-            </div>
-            <div className='py-10 text-center grid grid-cols-5 md:grid-cols-10'>
-                <span className='border px-6 py-2 bg-slate-100'>1</span>
-                <span className='border px-6 py-2 bg-slate-100'>2</span>
-                <span className='border px-6 py-2 bg-slate-100'>3</span>
-                <span className='border px-6 py-2 bg-slate-100'>4</span>
-                <span className='border px-6 py-2 bg-slate-100'>5</span>
-                <span className='border px-6 py-2 bg-slate-100'>6</span>
-                <span className='border px-6 py-2 bg-slate-100'>7</span>
-                <span className='border px-6 py-2 bg-slate-100'>8</span>
-                <span className='border px-6 py-2 bg-slate-100'>9</span>
-                <span className='border px-6 py-2 bg-slate-100'>10</span>
-            </div>
-        </section>
+        <div>
+            <section className='py-8 p-4'>
+                <h2 className='text-center text-xl p-4 font-bold'>Please Search Rockets</h2>
+                <div className='md:flex justify-evenly items-center py-8 p-4 gap-3 text-center'>
+                    <input
+                        value={searchOne}
+                        onChange={handleSearchOne}
+                        type='text' className='py-4 w-full p-2 mb-2 rounded-md bg-slate-100'
+                        placeholder='Find Rockets By Status'
+                    />
+                    <input
+                        value={searchTwo}
+                        onChange={handleSearchTwo}
+                        type='text'
+                        className='py-4 w-full p-2 mb-2 rounded-md bg-slate-100'
+                        placeholder='Find Rockets By Original_launch'
+                    />
+                    <input
+                        value={searchThree}
+                        onChange={handleSearchThree}
+                        type='text'
+                        className='py-4 w-full p-2 mb-2 rounded-md bg-slate-100'
+                        placeholder=' Rockets By Type' />
+                </div>
+            </section>
+            <section className='py-10 p-4'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4'>
+                    {
+                        filteredCapsules.map(capsule => <div key={capsule.capsule_serial} className='text-left border p-2 rounded-lg shadow-lg shadow-slate-500/50'>
+                            <p className='text-lg'>Status: {capsule.status}</p>
+                            <p className='text-lg'> Original Launch: {capsule.original_launch}</p>
+                            <p className='text-lg'>Missions: {capsule.missions[0]?.name}</p>
+                            <p className='text-lg'>Landing: {capsule.landings}</p>
+                            <p className='text-lg'>Type: {capsule.type}</p>
+                            <div className='py-6'>
+                                <button onClick={() => handleClick(capsule)} className='w-full rounded-md text-white bg-slate-950 hover:bg-indigo-700 p-4 text-sm'>View Details</button>
+                                <CapsuleModal isOpen={isModalOpen} onClose={handleModalClose} capsule={singleCapsule}></CapsuleModal>
+                            </div>
+                        </div>)
+                    }
+                </div>
+                <div className='py-10 text-center grid grid-cols-5 md:grid-cols-10'>
+                    <span className='border px-6 py-2 bg-slate-100'>1</span>
+                    <span className='border px-6 py-2 bg-slate-100'>2</span>
+                    <span className='border px-6 py-2 bg-slate-100'>3</span>
+                    <span className='border px-6 py-2 bg-slate-100'>4</span>
+                    <span className='border px-6 py-2 bg-slate-100'>5</span>
+                    <span className='border px-6 py-2 bg-slate-100'>6</span>
+                    <span className='border px-6 py-2 bg-slate-100'>7</span>
+                    <span className='border px-6 py-2 bg-slate-100'>8</span>
+                    <span className='border px-6 py-2 bg-slate-100'>9</span>
+                    <span className='border px-6 py-2 bg-slate-100'>10</span>
+                </div>
+            </section>
+        </div>
     );
 };
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-import Search from '../Search/Search';
 import Capsules from '../Capsules/Capsules';
 
 const Home = () => {
     return (
         <div className='xl:px-32'>
             <Banner></Banner>
-            <Search></Search>
             <Capsules></Capsules>
         </div>
     );
